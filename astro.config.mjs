a0bd1d1e118ed78024c91db1252331094972f834
Tomek Marciniak <tmarciniakm@gmail.com>
2024-09-01 16:21:36 +0200

    chore: increase scores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import robots from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://palladians.xyz",
-	integrations: [tailwind()],
+	integrations: [tailwind(), robots(), sitemap()],
 });
